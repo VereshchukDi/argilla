163cb31c89eae39b98ad82c15db57223b6af27e2
Dina Vereshchuk <vereshchukd1983@outlook.com>
2024-06-23 19:39:38 +0300

    Committing file argilla-frontend/components/base/base-table/BaseTableInfo.vue

---
+++ argilla-frontend/components/base/base-table/BaseTableInfo.vue
@@ -0,0 +1,485 @@
+<!--
+  - coding=utf-8
+  - Copyright 2021-present, the Recognai S.L. team.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+<template>
+  <transition appear name="fade">
+    <div class="table-info">
+      <div class="table-info__header">
+        <slot name="columns">
+          <div class="table-info__item">
+            <div
+              v-for="(column, key) in columns"
+              :key="key"
+              :class="[`table-info__item__col`, column.class]"
+            >
+              <lazy-table-filtrable-column
+                :column="column"
+                :data="data"
+                :filters="filters"
+                v-if="column.filtrable"
+                @applyFilters="onApplyFilters"
+              />
+              <button
+                v-else-if="column.sortable"
+                :data-title="column.tooltip"
+                :class="[sortOrder, { active: sortedBy === column.field }]"
+                @click="sort(column)"
+              >
+                <svgicon width="18" height="18" color="#4D4D4D" name="sort" />
+                <span>{{ column.name }}</span>
+              </button>
+              <button v-else :data-title="column.tooltip">
+                <span>{{ column.name }}</span>
+              </button>
+            </div>
+          </div>
+        </slot>
+      </div>
+      <results-empty v-if="tableIsEmpty" :title="emptySearchInfo.title" />
+      <template v-else>
+        <div class="table-info__body" ref="table">
+          <ul>
+            <li v-for="item in filteredResults" :key="item.id" :id="item.id">
+              <nuxt-link :to="rowLink(item)" class="table-info__item">
+                <span
+                  v-for="(column, idx) in columns"
+                  :key="idx"
+                  :class="[`table-info__item__col`, column.class]"
+                >
+                  <span :class="column.class">
+                    <span v-if="column.actions">
+                      <div class="table-info__actions">
+                        <p
+                          class="table-info__main"
+                          v-if="column.type === 'main'"
+                        >
+                          {{ itemValue(item, column) }}
+                        </p>
+                        <span v-else>{{ itemValue(item, column) }}</span>
+                        <div class="table-info__actions__buttons">
+                          <base-action-tooltip
+                            v-for="(action, idx) in column.actions"
+                            :key="idx"
+                            :tooltip="action.tooltip"
+                          >
+                            <base-button
+                              :title="action.title"
+                              class="table-info__actions__button button-icon"
+                              @click.prevent="
+                                onActionClicked(action.name, item)
+                              "
+                            >
+                              <svgicon
+                                v-if="action.icon !== undefined"
+                                :name="action.icon"
+                                width="16"
+                                height="16"
+                              />
+                            </base-button>
+                          </base-action-tooltip>
+                        </div>
+                      </div>
+                    </span>
+                    <base-date
+                      v-else-if="column.type === 'date'"
+                      format="date-relative-now"
+                      :date="itemValue(item, column)"
+                    />
+                    <nuxt-link v-else-if="column.link" :to="column.link(item)">
+                      {{ itemValue(item, column) }}
+                    </nuxt-link>
+                    <span v-else>{{ itemValue(item, column) }}</span>
+                    <span v-if="column.component">
+                      <component
+                        v-if="hydrate[item.id]"
+                        :is="column.component.name"
+                        v-bind="{ ...column.component.props(item) }"
+                      />
+                    </span>
+                  </span>
+                </span>
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </template>
+    </div>
+  </transition>
+</template>
+
+<script>
+import "assets/icons/trash-empty";
+import "assets/icons/refresh";
+import "assets/icons/copy";
+import "assets/icons/settings";
+import "assets/icons/link";
+import "assets/icons/sort";
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+    columns: {
+      type: Array,
+      required: true,
+    },
+    sortedOrder: {
+      type: String,
+      default: "desc",
+    },
+    searchOn: {
+      type: String,
+      required: true,
+    },
+    sortedByField: {
+      type: String,
+      default: undefined,
+    },
+    emptySearchInfo: {
+      title: undefined,
+    },
+    querySearch: {
+      type: String,
+      default: undefined,
+    },
+    activeFilters: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    rowLink: {
+      type: Function,
+      default: () => {
+        return () => {};
+      },
+    },
+  },
+  data() {
+    return {
+      sortOrder: this.sortedOrder,
+      sortedBy: this.sortedByField,
+      filters: {},
+      hydrate: {},
+    };
+  },
+  watch: {
+    filteredResults() {
+      this.changeVisibility();
+    },
+  },
+  computed: {
+    tableIsEmpty() {
+      return this.filteredResults && this.filteredResults.length === 0;
+    },
+    filterActions() {
+      return this.actions;
+    },
+    filteredResults() {
+      const matchSearch = (item) => {
+        if (this.querySearch === undefined) {
+          return true;
+        }
+        const querySearch = this.querySearch.toLowerCase();
+
+        return item[this.searchOn]
+          .toString()
+          .toLowerCase()
+          .includes(querySearch);
+      };
+      const matchFilters = (item) => {
+        if (this.filters) {
+          return Object.keys(this.filters).every((key) => {
+            return this.filters[key].includes(item[key]);
+          });
+        }
+
+        return true;
+      };
+      const itemComparator = (a, b) => {
+        const modifier = this.sortedOrder === "desc" ? -1 : 1;
+
+        if (a[this.sortedBy] < b[this.sortedBy]) return -1 * modifier;
+        if (a[this.sortedBy] > b[this.sortedBy]) return 1 * modifier;
+
+        return 0;
+      };
+
+      return this.data
+        .filter(matchSearch)
+        .filter(matchFilters)
+        .sort(itemComparator);
+    },
+  },
+  beforeMount() {
+    this.sortedBy = this.sortedByField;
+
+    return this.activeFilters
+      .filter((filter) => filter.values.length)
+      ?.forEach(({ column, values }) => {
+        this.$set(this.filters, column, values);
+      });
+  },
+  methods: {
+    itemValue(item, column) {
+      if (column.subfield) {
+        return item[column.field][column.subfield];
+      }
+      return item[column.field];
+    },
+    onActionClicked(action, item) {
+      this.$emit("onActionClicked", action, item);
+    },
+    sort(column) {
+      this.$emit("sort-column", column.field, this.sortOrder);
+      this.sortedBy = column.field;
+      if (column.field === this.sortedBy) {
+        this.sortOrder = this.sortOrder === "asc" ? "desc" : "asc";
+      }
+    },
+    onApplyFilters(column, selectedOptions) {
+      if (selectedOptions.length) {
+        this.$set(this.filters, column.field, selectedOptions);
+      } else {
+        this.$delete(this.filters, column.field);
+      }
+      this.$emit("filter-applied", {
+        column: column.field,
+        values: selectedOptions,
+      });
+    },
+    changeVisibility() {
+      const handleIntersection = (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            this.$set(this.hydrate, entry.target.id, true);
+          }
+        }
+      };
+
+      const observer = new IntersectionObserver(handleIntersection);
+
+      this.data.forEach((item) => {
+        const element = document.getElementById(item.id);
+        if (!element) return;
+
+        observer.observe(element);
+      });
+    },
+  },
+  mounted() {
+    this.changeVisibility();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$headerColor: palette(grey, 700);
+$borderColor: palette(grey, 600);
+
+.table-info {
+  $this: &;
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  min-height: 0;
+  list-style: none;
+  color: $black-54;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  &__header {
+    border: 1px solid $borderColor;
+    border-bottom: 0;
+    border-radius: var(--m, 10px) var(--m, 10px) 0px 0px;
+    background: $headerColor;
+    min-height: 49px;
+    position: relative;
+    margin-top: $base-space * 2;
+    .table-info:has(.empty) & {
+      border-bottom: 1px solid $borderColor;
+    }
+    &__checkbox {
+      margin: 0 !important;
+    }
+    &__button {
+      position: absolute !important;
+      top: 2em;
+      right: 2em;
+      margin-bottom: 0 !important;
+    }
+    #{$this}__item {
+      border: unset;
+      min-height: 50px;
+      background: none;
+      padding-top: 0.2em;
+      padding-bottom: 0.2em;
+      display: flex;
+      align-items: center;
+    }
+    button:not(.button) {
+      cursor: pointer;
+      border: 0;
+      outline: none;
+      background: transparent;
+      padding-left: 0;
+      padding-right: 0;
+      color: $black-87;
+      @include font-size(14px);
+      text-align: left;
+      display: flex;
+      align-items: center;
+      @include media("<=desktop") {
+        display: block;
+        .svg-icon {
+          display: block;
+        }
+      }
+      span {
+        white-space: nowrap;
+      }
+    }
+  }
+  &__body {
+    overflow: auto;
+    padding-bottom: 0.5em;
+    min-height: 250px;
+    @extend %hide-scrollbar;
+    #{$this}__item {
+      &:hover,
+      &:focus {
+        background: #fcfcfc;
+      }
+    }
+  }
+  &__item {
+    position: relative;
+    display: flex;
+    align-items: center;
+    background: palette(white);
+    list-style: none;
+    padding: $base-space * 2 $base-space * 2;
+    width: 100%;
+    min-height: $base-space * 10;
+    text-decoration: none;
+    outline: none;
+    border: 1px solid $borderColor;
+    border-top: 0;
+
+    &__col {
+      text-align: left;
+      margin-right: 1em;
+      flex: 1 1 0px;
+      &:nth-last-of-type(-n + 1) {
+        max-width: 120px;
+      }
+      &:first-child {
+        width: auto;
+        min-width: auto;
+        flex-grow: 2.5;
+      }
+      &.progress {
+        min-width: 160px;
+      }
+    }
+    .svg-icon {
+      margin-right: $base-space;
+      fill: $black-37;
+      &:hover {
+        fill: $black-54;
+      }
+    }
+  }
+  .empty {
+    margin-top: 5em;
+    height: auto;
+    min-height: 50vh;
+  }
+  &__main {
+    margin: 0;
+    color: $black-87;
+    .table-info__item:hover & {
+      color: $primary-color;
+    }
+  }
+  &__actions {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: center;
+    gap: $base-space;
+
+    &__buttons {
+      display: flex;
+      flex-shrink: 0;
+
+      &__button {
+        position: relative;
+        display: inline-block;
+        .svg-icon {
+          margin: 0;
+        }
+        & + #{$this} {
+          margin-left: auto;
+        }
+      }
+    }
+  }
+  &__title {
+    display: block;
+    hyphens: auto;
+    word-break: break-word;
+    .button-icon {
+      padding: $base-space;
+      display: inline-block;
+      .svg-icon {
+        margin: 0;
+      }
+    }
+    a {
+      text-decoration: none;
+      &:hover {
+        color: palette(black);
+      }
+    }
+  }
+  .text {
+    color: $black-54;
+    p {
+      display: inline-block;
+      background: palette(grey, 800);
+      padding: 0.5em;
+      border-radius: 10px;
+      margin-right: 0.5em;
+      margin-top: 0;
+      hyphens: auto;
+      word-break: break-word;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+}
+button[data-title] {
+  position: relative;
+  @extend %has-tooltip--top;
+  @extend %tooltip-large-text;
+}
+</style>
